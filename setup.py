6084f4c66022a3696cfb62b50ffcba167ed5f332
Shadow Ash <139130225+shadow7ash@users.noreply.github.com>
2024-05-20 04:47:35 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+# Dummy setup.py script
+# This script does nothing and is used only to satisfy Heroku's postdeploy script requirement
+
+def main():
+    print("Dummy setup.py script executed.")
+
+if __name__ == "__main__":
+    main()
