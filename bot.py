d9114969bf20d86073d9551c7515328aa89ba901
Shadow Ash <139130225+shadow7ash@users.noreply.github.com>
2024-05-21 02:14:26 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -143,5 +143,4 @@ async def start_application() -> None:
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(start_application())
+    asyncio.run(start_application())
