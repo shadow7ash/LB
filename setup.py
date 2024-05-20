459ee987c64a9167c1635e9a6ce69682f4c56eba
Shadow Ash <139130225+shadow7ash@users.noreply.github.com>
2024-05-20 22:37:40 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='your-bot-name',
-    version='1.0',
-    packages=[],
+    name="sa-leech-bot",
+    version="1.0",
+    packages=find_packages(),
     install_requires=[
-        'python-telegram-bot==14.2',
-        'pymongo==4.0.1',
-        'requests==2.26.0'
+        "python-telegram-bot==20.0",
+        "pymongo==3.12.1",
+        "requests==2.31.0"
     ],
     entry_points={
-        'console_scripts': []
+        "console_scripts": [
+            "start-bot=bot:main",
+        ],
     },
-    python_requires='>=3.12.3',
 )
