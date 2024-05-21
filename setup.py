fe1ae2740fa7fed9f613c2297042715aee5fafff
Shadow Ash <139130225+shadow7ash@users.noreply.github.com>
2024-05-21 23:20:12 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     packages=find_packages(),
     install_requires=[
-        'python-telegram-bot==13.5',
+        'python-telegram-bot>=20.0',
         'pymongo[srv]==4.0',
         'requests==2.25.1',
         'dnspython'
